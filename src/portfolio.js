18dd198b1550f265cdb5256063fd1e96971a3c24
N-Mendiola <nm58@rice.edu>
2025-07-21 23:04:08 -0500

    added flair

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,71 +1,86 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://N-mendiola.github.io',
+  title: 'Natalia Mendiola.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Natalia Mendiola',
+  role: 'Graduate Student at Georgia Tech',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    "My name is Natalia Mendiola and I am a graduate student at Georgia Tech majoring in Electrical and Computer Engineering. I'm excited to shape data-driven, human-centered products that create meaningful impact. I thrive in collaborative, fast-paced environments and love working with people from diverse backgrounds to build what's next.",
+  //resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/natalia-mendiola/',
+    github: 'https://github.com/N-Mendiola',
   },
 }
+
+const experiences = [
+  {
+    role: 'Power Electronics - Vehicle Hardware Intern	',
+    company: 'Tesla',
+    date: 'May 2024 - August 2024',
+    description: `Collaborated cross-functionally with engineering and data infrastructure teams to identify inefficiencies in failure analysis workflows. Proposed, scoped, and led integration of a new Power BI dashboard into existing workflows, streamlining cross-team hardware failure analysis and saving 48+ engineering hours/month.Built OpenCV pipeline to detect PCB design changes in visual schematics, aiding design traceability and reverse comparison, reducing review time by 65%.`,
+  },
+  {
+    role: 'Electrical Engineering R&D Intern',
+    company: 'Boston Scientific',
+    date: 'June 2023 - August 2023',
+    description: `Led end-to-end program coordination for a design change initiative, synthesizing feedback from 6+ stakeholder teams and publishing a technical impact assessment to guide decision-making. Extended product availability for another 2 years through technical assessment of electrical component datasheet specifications, identified replacements for end-of-life components. `,
+  },
+]
 
 const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'BSECE Capstone Project: Ghost In the Machine',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Collaborated with a team of 7 to develop a highly parameterized deep neural network accelerator with systolic array architecture featuring novel processing elements designed to improve computation throughput. Implemented and evaluated the accelerator design in Simulink, Vivado, and FPGA for analysis and benchmarking.',
+    stack: ['C++', 'Python', 'Vitis HLS', 'Xilinx Vivado', 'Agile Methodology'],
+    //sourceCode: 'https://github.com',
+    livePreview: 'https://www.linkedin.com/posts/pchari_feeling-incredibly-proud-for-winning-the-activity-7319114783046127617-YF4T?utm_source=share&utm_medium=member_desktop&rcm=ACoAADk8N1EBcFOda2nCt1j_nPaLrondlVhI5Pg',
   },
   {
-    name: 'Project 2',
+    name: 'Autonomous Lane Keeping RC Car',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Developed an autonomous driving system for an RC car using Python, implementing lane-keeping, stop-sign detection.',
+    stack: ['Python', 'OpenCV', 'Raspberry PI', 'Embedded Linux'],
+    sourceCode: 'www.hackster.io/projects/00b157/',
+    livePreview: 'https://youtu.be/R4vvM8UXXF4?si=R2jtk4mRyPFazQtj',
   },
   {
-    name: 'Project 3',
+    name: 'Guitar Hero with MSP430',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Designed and implemented Guitar Hero gameplay using MSP430 microcontrollers, a rhythm-based game with real-time logic, LED visualizations, audio processing, and dynamic user interactions on TI LaunchPad Kits.',
+    stack: ['C', 'MSP430', 'Embedded Systems'],
+    sourceCode: 'https://github.com/lindseyr23/GuitarHero430', 
+    livePreview: 'https://youtu.be/caU7hopNlVo?si=VWlUrG9y-vQojxJ4',
   },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Python',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Verilog',
+  'C',
+  'MATLAB',
+  'PCB Design',
+  'Altium',
+  'Vitis HLS',
+  'PostgreSQL',
+  'R',
+  'LTSpice',
+  'Embedded Linux'
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'natmendiolav@mail.com',
 }
 
-export { header, about, projects, skills, contact }
+export { header, about, contact, projects, experiences, skills }
